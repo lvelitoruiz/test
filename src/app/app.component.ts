c3f53b1e577a375e63b418ea326d48e51e98e581
lvelitoserempre <jvelito@serempre.com>
2021-09-10 13:14:23 -0500

    changes to main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { formattedError } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'circles';
+  public items: Array<any> = [];
+
+  constructor() {
+    this.items = [
+      {
+        status: 'active',
+        id: 'firstItem'
+      },
+      {
+        status: 'former',
+        id: 'secondItem'
+      },
+      {
+        status: '',
+        id: 'thirdItem'
+      },      {
+        status: '',
+        id: 'fourthItem'
+      }
+    ]
+  }
+
+  ngOnInit():void {
+
+  }
+
+  changeProperty(id: string): void {
+    this.items.forEach((item) => {
+      if(item.status == 'active') {
+        item.status = 'former';
+      } else {
+        item.status = '';
+      }
+      if(item.id == id) {
+        item.status = 'active';
+      }
+    })
+  }
 }
